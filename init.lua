a5200e14911c294b6b998ee6ca170ea0a1cea533
Jack <jdonnelly@chapman.edu>
2025-03-01 20:54:30 -0600

    updated init.lua

--- init.lua
+++ init.lua
@@ -337,6 +337,27 @@ require('lazy').setup({
     'nvim-tree/nvim-tree.lua',
     opts = {},
   },
+  {
+    'stevearc/oil.nvim',
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {
+      columns = { 'icon' },
+      keymaps = {
+        ['<C-h>'] = false,
+        ['<M-h>'] = 'actions.select_split',
+      },
+      view_options = {
+        show_hidden = true,
+      },
+      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' }),
+    },
+    -- Optional dependencies
+    dependencies = { { 'nvim-tree/nvim-web-devicons', opts = {} } },
+    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+    -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+    lazy = false,
+  },
 
   -- NOTE: Plugins can specify dependencies.
   --
